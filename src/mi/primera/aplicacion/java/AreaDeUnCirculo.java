0b8359aad5e458848ed1d52dcb4c3b21272572a7
SalvaVE <valladares.salvador12@gmail.com>
2022-03-08 10:48:59 -0600

    PrimerProyectoJava

---
+++ src/mi/primera/aplicacion/java/AreaDeUnCirculo.java
@@ -0,0 +1,9 @@
+
+package mi.primera.aplicacion.java;
+
+public class AreaDeUnCirculo {
+    public static void main (String[] args){
+        
+    }
+ }
+
---
+++ src/mi/primera/aplicacion/java/CalculoInteresSimple.java
@@ -0,0 +1,20 @@
+
+package mi.primera.aplicacion.java;
+    
+
+public class CalculoInteresSimple {
+    public static void main (String[] args){
+        double interes, capitalPrestado, tiempo, tasadeinteres, tiempoenmeses;
+    
+    interes = 144;
+    capitalPrestado =  1200.00;
+    tasadeinteres = 0.08;
+    tiempo = interes / (capitalPrestado * tasadeinteres);
+    tiempoenmeses = tiempo * 12;
+    System.out.println("Tiempo: " + tiempo + "años");
+    System.out.println("Tiempo: " + tiempoenmeses + "meses");
+}
+    
+}
+    
+
